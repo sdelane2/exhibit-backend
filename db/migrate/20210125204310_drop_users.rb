41e5386ecda7d28e8be7990a297922878129057f
sdelane2 <seanmartindelaney@gmail.com>
2022-02-08 11:55:23 -0500

    changing to ternary

--- db/migrate/20210125204310_drop_users.rb
+++ db/migrate/20210125204310_drop_users.rb
@@ -1,6 +1,6 @@
 class DropUsers < ActiveRecord::Migration[6.0]
   def change
-    drop_table (:users, if_exists: true)
-    drop_table (:favorite_artworks, if_exists: true)
+    table_exists?(:users) ? drop_table(:users) : nil
+    table_exists?(:favorite_artworks) ? drop_table(:favorite_artworks) : nil
   end
 end
