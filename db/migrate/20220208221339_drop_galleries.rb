11816fbcc468e6ea39794380215ec375dc4fc248
sdelane2 <seanmartindelaney@gmail.com>
2022-02-08 17:54:01 -0500

    reformat migration

--- db/migrate/20220208221339_drop_galleries.rb
+++ db/migrate/20220208221339_drop_galleries.rb
@@ -1,7 +1,7 @@
 class DropGalleries < ActiveRecord::Migration[6.0]
   def change
     table_exists?(:galleries) ? drop_table(:galleries) : nil
-    table_exists?(:exhibitions) ? add_column(:user_id, :integer) : nil
-    table_exists?(:gallery_artworks) ? add_column(:user_id, :integer) : nil
+    table_exists?(:exhibitions) ? add_column(:exhibitions, :user_id, :integer) : nil
+    table_exists?(:gallery_artworks) ? add_column(:gallery_artworks, :user_id, :integer) : nil
   end
 end
