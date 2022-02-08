3ed4a9b5177a1170567457352d209844b2af6884
sdelane2 <seanmartindelaney@gmail.com>
2022-02-08 11:53:53 -0500

    update drop users

--- db/migrate/20210125204310_drop_users.rb
+++ db/migrate/20210125204310_drop_users.rb
@@ -1,6 +1,6 @@
 class DropUsers < ActiveRecord::Migration[6.0]
   def change
-    drop_table :users 
-    drop_table :favorite_artworks
+    drop_table (:users, if_exists: true)
+    drop_table (:favorite_artworks, if_exists: true)
   end
 end
