571bf40392f4e69d65b7397b497f96cd4ac68c81
sdelane2 <seanmartindelaney@gmail.com>
2022-02-08 17:35:00 -0500

    adding integer to remove_column

--- db/migrate/20220208221339_drop_galleries.rb
+++ db/migrate/20220208221339_drop_galleries.rb
@@ -1,9 +1,9 @@
 class DropGalleries < ActiveRecord::Migration[6.0]
   def change
     table_exists?(:galleries) ? drop_table(:galleries) : nil
-    table_exists?(:exhibitions) ? remove_column(:gallery_id) : nil
+    table_exists?(:exhibitions) ? remove_column(:gallery_id, :integer) : nil
     table_exists?(:exhibitions) ? add_column(:user_id, :integer) : nil
-    table_exists?(:gallery_artworks) ? remove_column(:gallery_id) : nil
+    table_exists?(:gallery_artworks) ? remove_column(:gallery_id, :integer) : nil
     table_exists?(:gallery_artworks) ? add_column(:user_id, :integer) : nil
   end
 end
