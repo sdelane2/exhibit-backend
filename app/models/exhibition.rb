5b66a27309e56ea184925b503a2a776c9ad43a97
sdelane2 <seanmartindelaney@gmail.com>
2022-04-27 17:42:19 -0400

    Remove image function from Exhibition model

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -4,20 +4,4 @@ class Exhibition < ApplicationRecord
     belongs_to :user
     has_many :favorite_exhibitions
     
-    
-    def image
-        if self.exhibited_artworks.count > 0
-            self.exhibited_artworks.map do |artwork|
-                if artwork.cover_image === true
-                    return artwork.gallery_artwork.image_url
-                else
-                    return artwork.gallery_artwork.image_url
-                end
-            end
-        else
-            return "https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png"
-        end
-    end
-    
-
 end
